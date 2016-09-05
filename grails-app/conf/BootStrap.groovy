aea4d970aba91d14d514a7c01c93bc99a2724819
ravi-jft <ravi.kumar@jellyfishtechnologies.com>
2016-09-05 15:02:45 +0530

    Ravi first commit

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,19 @@
+
+class BootStrap {
+
+    def init = { servletContext ->
+        def adminRole = new SecAppRole(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new SecAppRole(authority: 'ROLE_USER').save(flush: true)
+
+        def testUser = new SecAppUser(username: 'admin', enabled: true, password: 'admin')
+        testUser.save(flush: true)
+
+        SecAppUserSecAppRole.create testUser, adminRole, true
+
+        assert SecAppUser.count() == 1
+        assert SecAppRole.count() == 2
+        assert SecAppUserSecAppRole.count() == 1
+    }
+    def destroy = {
+    }
+}
---
+++ grails-app/controllers/com/bahmanm/secureapp/SensitiveContentController.groovy
@@ -0,0 +1,13 @@
+package com.bahmanm.secureapp
+import org.springframework.security.access.annotation.Secured
+/*
+ If you're using older Grails version like 2.2.x series use the
+ following instead:
+   import grails.plugin.springsecurity.annotation.Secured
+ */
+class SensitiveContentController {
+    @Secured(['ROLE_ADMIN'])
+    def index() {
+        render "Some sensitive content"
+    }
+}
